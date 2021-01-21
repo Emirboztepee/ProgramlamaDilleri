9e3c59b38db2001c6d71f00dd8c1474cf42bd33b
Emir Boztepe <61057157+Emirboztepee@users.noreply.github.com>
2021-01-22 00:25:36 +0300

    a
    a

--- app/src/main/java/com/hr190012/emirdursunboztepefinal/ProgramlamaApplication.java
+++
@@ -1,7 +0,0 @@
-package com.hr190012.emirdursunboztepefinal;
-
-import android.app.Application;
-
-public class ProgramlamaApplication extends Application {
-
-}
--- app/src/main/java/com/hr190012/emirdursunboztepefinal/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.hr190012.emirdursunboztepefinal.util;
-
-public class Constants {
-
-    public  static String TIKLANAN_DIL_TASINANIN_BASLIGI ="tiklananDil";
-
-    public  static String BASE_URL = "https://raw.githubusercontent.com/Emirboztepee/ProgramlamaDilleri/main/";
-
-}
